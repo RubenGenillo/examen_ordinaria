5ed9f878538263a97140e11bf31d5ea94dcef1dd
RubenGenillo <114146893+RubenGenillo@users.noreply.github.com>
2022-12-19 19:32:09 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from ejercicio3 import main as main3
+from ejercicio4 import main as main4
+from ejercicio5 import main as main5
+from ejercicio8 import main as main8
+
+if __name__ == '__main__':
+    main3()
+    main4()
+    main5()
+    main8()
